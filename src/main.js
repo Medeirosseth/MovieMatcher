578a5a51c5d1fa0353303a1123f5755d1224a82a
Ryan Walker <ryandanielwalker@gmail.com>
2021-04-26 16:49:01 -0700

    WIP: Styling, add logo and adjust colors

--- src/main.js
+++ src/main.js
@@ -2,3 +2,43 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import MovieService from './js/api.js';
+
+
+
+
+///// checklist
+// user constructors
+// movie databases
+// API
+// function that compares users movies liked
+// yes - pushes movie title to the liked movie array,  
+
+
+
+
+
+function getElements(response) {
+  console.log(response);
+  $("#showPoster").html(`<img src="https://image.tmdb.org/t/p/w500/${response.results[0].poster_path}"/>`)
+}
+
+
+$(document).ready(function () {
+  const movieArray = ["Fight club", "Ghostbusters"];
+  let currentMovie = movieArray[0];
+  MovieService.getMovieInfoAPI(currentMovie)
+    .then(function (response) {
+      getElements(response);
+
+      $("#button").click(function () {
+        // currentMovie = movieArray[i] += 1;
+        console.log(currentMovie);
+        MovieService.getMovieInfoAPI(currentMovie)
+          .then(function (response) {
+            getElements(response);
+          });
+      });
+
+    });
+});
