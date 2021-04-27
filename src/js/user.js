3f4b4be075514effdf4cc6ded180480b5d6cba5f
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-04-27 11:52:50 -0700

    add user sign up and switch user functionality

--- src/js/user.js
+++ src/js/user.js
@@ -1,10 +1,25 @@
 export default class User {
-  constructor(userName) {
+  constructor(userID, userName) {
+    this.userID = userID;
     this.userName = userName;
-    this.movieArray = ['The Fast and the Furious', '2 Fast 2 Furious', 'The Fast and The Furious: Tokyo Drift', 'Fast & Furious', 'Fast Five, Fast & Furious 6, Furious 7', 'The Fate of the Furious', 'Fast & Furious Presents: Hobbs & Shaw', 'Fast & Furious 9'];
+    this.movieArray = [
+      "The Fast and the Furious",
+      "2 Fast 2 Furious",
+      "The Fast and The Furious: Tokyo Drift",
+      "Fast & Furious",
+      "Fast Five, Fast & Furious 6, Furious 7",
+      "The Fate of the Furious",
+      "Fast & Furious Presents: Hobbs & Shaw",
+      "Fast & Furious 9",
+    ];
     this.moviesLiked = [];
     // this.userID = 0;
   }
+
+  // addUser() {
+  //   userID = this.assignID();
+  //   this.userID.push(user);
+  // }
 
   // assignID() {
   //   this.userID += 1;
--- src/main.js
+++ src/main.js
@@ -9,53 +9,61 @@ let user1;
 let user2;
 let currentUser;
 
-
-
 function switchUser() {
-  if (currentUser.userName === "user1") {
+  if (currentUser.userID === 1) {
     currentUser = user2;
   } else {
     currentUser = user1;
   }
 }
 
-
 function getElements(response) {
   console.log(response);
-  $("#showPoster").html(`<img src="https://image.tmdb.org/t/p/w500/${response.results[0].poster_path}"/>`);
+  $("#showPoster").html(
+    `<img src="https://image.tmdb.org/t/p/w500/${response.results[0].poster_path}"/>`
+  );
 }
 
 $(document).ready(function () {
   user1 = new User("user1");
+  user1.userID = 1;
   user2 = new User("user2");
+  user2.userID = 2;
   currentUser = user1;
   let currentMovie = currentUser.movieArray[0];
   console.log(currentUser.movieArray);
-  MovieService.getMovieInfoAPI(currentMovie)
-    .then(function (response) {
-      getElements(response);
-    });
+  MovieService.getMovieInfoAPI(currentMovie).then(function (response) {
+    getElements(response);
+  });
 
+  $("#inputForm").submit(function (event) {
+    event.preventDefault();
+    currentUser.userName = $("#userNameInput").val();
+    console.log("our current user: ", currentUser);
+    $("#showMovies").toggle();
+    $(".userInput").slideToggle();
+  });
 
   $("#yes").click(function () {
     currentMovie = currentUser.movieArray[0];
     currentUser.moviesLiked.push(currentMovie);
     currentUser.movieArray.shift();
     console.log(currentUser.moviesLiked);
-    MovieService.getMovieInfoAPI(currentMovie)
-      .then(function (response) {
-        getElements(response);
-      });
+    MovieService.getMovieInfoAPI(currentMovie).then(function (response) {
+      getElements(response);
+    });
   });
   $("#no").click(function () {
     currentUser.movieArray.shift();
-    MovieService.getMovieInfoAPI(currentMovie)
-      .then(function (response) {
-        getElements(response);
-      });
+    MovieService.getMovieInfoAPI(currentMovie).then(function (response) {
+      getElements(response);
+    });
   });
 
   $("#switch").click(function () {
     switchUser();
+    $("#showMovies").toggle();
+    $(".userInput").slideToggle();
+    console.log(currentUser);
   });
 });
