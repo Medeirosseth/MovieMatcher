40e0e1216772027457be9299f24aa5556a588245
Ryan Walker <ryandanielwalker@gmail.com>
2021-04-27 09:55:05 -0700

    Add array functionality for liked movies

--- src/js/api.js
+++ src/js/api.js
@@ -1,8 +1,6 @@
 export default class MovieService {
-  static getMovieInfoAPI(currentMovie) {
-    return fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=en-US&query=${currentMovie}&page=1&include_adult=false`
-    )
+  static getMovieInfoAPI() {
+    return fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=en-US&query=Crash&page=1&include_adult=false`)
       .then(function (response) {
         if (!response.ok) {
           throw Error(response.statusText);
--- src/main.js
+++ src/main.js
@@ -3,18 +3,21 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import MovieService from "./js/api.js";
-import User from "./js/user";
+import User from "./js/user.js";
+
+let user1;
+// let user2;
+let currentUser;
 
 
 
-
-function switchUser() {
-  if (currentUser.userName === "user1") {
-    currentUser = user2;
-  } else {
-    currentUser = user1;
-  }
-}
+// function switchUser() {
+//   if (currentUser.userName === "user1") {
+//     currentUser = user2;
+//   } else {
+//     currentUser = user1;
+//   }
+// }
 
 
 function getElements(response) {
@@ -23,30 +26,32 @@ function getElements(response) {
 }
 
 $(document).ready(function () {
-  user1 = new User(userName);
-  user2 = new User(userName);
+  user1 = new User("user1");
+  // user2 = new User(userName);
   currentUser = user1;
   let currentMovie = currentUser.movieArray[0];
+  console.log(currentUser.movieArray);
   MovieService.getMovieInfoAPI(currentMovie)
-    .then(function () {
+    .then(function (response) {
       getElements(response);
-    })
+    });
 
 
   $("#yes").click(function () {
+    currentMovie = currentUser.movieArray[0];
     currentUser.moviesLiked.push(currentMovie);
     currentUser.movieArray.shift();
-    console.log(currentUser.moviesLiked)
+    console.log(currentUser.moviesLiked);
     MovieService.getMovieInfoAPI(currentMovie)
       .then(function (response) {
         getElements(response);
-      })
-  })
+      });
+  });
   $("#no").click(function () {
     currentUser.movieArray.shift();
     MovieService.getMovieInfoAPI(currentMovie)
       .then(function (response) {
         getElements(response);
       });
-  })
+  });
 });
