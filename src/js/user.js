e1068f3877aa0e2c7313b7184dbff1ab986a5fe8
Ryan Walker <ryandanielwalker@gmail.com>
2021-04-27 18:05:32 -0700

    Add movie switch functionality

--- src/js/user.js
+++ src/js/user.js
@@ -18,21 +18,19 @@ export default class User {
     // this.userID = 0;
   }
 
-<<<<<<< HEAD
+
 
 }
 
-=======
-  // addUser() {
-  //   userID = this.assignID();
-  //   this.userID.push(user);
-  // }
+// addUser() {
+//   userID = this.assignID();
+//   this.userID.push(user);
+// }
 
-  // assignID() {
-  //   this.userID += 1;
-  //   return this.userID;
-  // }
-}
+// assignID() {
+//   this.userID += 1;
+//   return this.userID;
+// }
 
-// The Fast and the Furious, 2 Fast 2 Furious, The Fast and The Furious: Tokyo Drift, Fast & Furious, Fast Five, Fast & Furious 6, Furious 7, The Fate of the Furious, Fast & Furious Presents: Hobbs & Shaw, Fast & Furious 9
->>>>>>> backend
+
+
--- src/main.js
+++ src/main.js
@@ -18,11 +18,11 @@ function switchUser() {
 }
 
 function getElements(response) {
-  console.log("response: ", response);
+  console.log(currentUser, user1, user2);
   $("#movieTitle").text(response.results[0].original_title);
   $("#movieOverview").text(response.results[0].overview);
   $("#moviePoster").html(
-    `<img src="https://image.tmdb.org/t/p/w500${response.results[0].poster_path}"/>`
+    `<img src="https://image.tmdb.org/t/p/w500${response.results[0].poster_path}" id="poster"/>`
   );
 }
 
@@ -32,7 +32,7 @@ function getElements(response) {
 // }
 
 function compareMovies(currentMovie) {
-  if (currentUser.userName === "user1") {
+  if (currentUser.userID === 1) {
     user2.moviesLiked.forEach(function (element) {
       if (currentMovie.includes(element)) {
         alert("It's a match!");
@@ -63,15 +63,17 @@ $(document).ready(function () {
   $("#inputForm").submit(function (event) {
     event.preventDefault();
     currentUser.userName = $("#userNameInput").val();
+    $("#userNameInput").val("");
     $("#showMovies").toggle();
     $(".userInput").slideToggle();
   });
 
   $("#yes").click(function () {
-    currentMovie = currentUser.movieArray[0];
+    // let currentMovie = currentUser.movieArray[0];
     currentUser.moviesLiked.push(currentMovie);
     compareMovies(currentMovie);
     currentUser.movieArray.shift();
+    currentMovie = currentUser.movieArray[0];
     MovieService.getMovieInfoAPI(currentMovie)
       .then(function (response) {
         getElements(response);
@@ -79,17 +81,24 @@ $(document).ready(function () {
   });
 
   $("#no").click(function () {
+    // let currentMovie = currentUser.movieArray[0];
     currentUser.movieArray.shift();
-    MovieService.getMovieInfoAPI(currentMovie).then(function (response) {
-      getElements(response);
-    });
+    currentMovie = currentUser.movieArray[0];
+    MovieService.getMovieInfoAPI(currentMovie)
+      .then(function (response) {
+        getElements(response);
+      });
   });
 
   $("#switch").click(function () {
     switchUser();
+    currentMovie = currentUser.movieArray[0];
+    MovieService.getMovieInfoAPI(currentMovie)
+      .then(function (response) {
+        getElements(response);
+      });
     $("#showMovies").toggle();
     $(".userInput").slideToggle();
-    console.log(currentUser);
   });
 });
 
